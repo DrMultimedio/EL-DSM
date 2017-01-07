973c54e51a6e647889435cd75131a21557cd9380
Jose Manuel <coveromaki@gmail.com>
2017-01-07 18:10:29 +0100

    Añadido login

---
+++ ProyectoVikingsGen/ProyectoVikingsGenNHibernate/CEN/ProyectoVikings/JugadorCEN_Login.cs
@@ -0,0 +1,40 @@
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Criterion;
+using NHibernate.Exceptions;
+using ProyectoVikingsGenNHibernate.Exceptions;
+using ProyectoVikingsGenNHibernate.EN.ProyectoVikings;
+using ProyectoVikingsGenNHibernate.CAD.ProyectoVikings;
+
+
+/*PROTECTED REGION ID(usingProyectoVikingsGenNHibernate.CEN.ProyectoVikings_Jugador_login) ENABLED START*/
+//  references to other libraries
+/*PROTECTED REGION END*/
+
+namespace ProyectoVikingsGenNHibernate.CEN.ProyectoVikings
+{
+public partial class JugadorCEN
+{
+public bool Login (int p_oid, string password)
+{
+        /*PROTECTED REGION ID(ProyectoVikingsGenNHibernate.CEN.ProyectoVikings_Jugador_login) ENABLED START*/
+
+        // Write here your custom code...
+
+        bool result = false;
+        JugadorEN en = _IJugadorCAD.ReadOIDDefault (p_oid);
+
+        if (en.Contrasenya.Equals (Utils.Util.GetEncondeMD5 (contrasenya)))
+                result = true;
+        return result;
+
+        //throw new NotImplementedException ("Method Login() not yet implemented.");
+
+        /*PROTECTED REGION END*/
+}
+}
+}
